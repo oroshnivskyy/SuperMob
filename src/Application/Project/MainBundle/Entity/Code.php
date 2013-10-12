8a606c286a4e5fb9383de97af4ef3fb3ec9e6749
aklosovich <aklosovich@neoline.biz>
2013-10-12 14:41:06 +0300

    Страны которые мы будем обслуживать задем в конфиге для начала это 3 страны россия Украина белорусия
    Модель Operator (связик, пример: киевстар) код страны берем с конфига
    
    Модель Code. Список кодов пополнения мобильного счета.

---
+++ src/Application/Project/MainBundle/Entity/Code.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Application\Project\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Code
+ *
+ * @ORM\Table(name="code")
+ * @ORM\Entity
+ */
+class Code
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="code", type="integer")
+     */
+    private $code;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="price", type="integer")
+     */
+    private $price;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Operator", inversedBy="codes")
+     * @ORM\JoinColumn(name="operator_id", referencedColumnName="id")
+     */
+    private $operator;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param integer $code
+     * @return Code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return integer 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     * @return Code
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return boolean 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set Price
+     *
+     * @param integer $price
+     * @return Code
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    
+        return $this;
+    }
+
+    /**
+     * Get Price
+     *
+     * @return integer 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Code
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set Operator
+     *
+     * @param Operator $operator
+     * @return Code
+     */
+    public function setOperator(Operator $operator)
+    {
+        $this->operator = $operator;
+
+        return $this;
+    }
+
+    /**
+     * Get operator
+     *
+     * @return Operator|NULL
+     */
+    public function getOperator()
+    {
+        return $this->operator;
+    }
+}
