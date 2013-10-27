eddbd134d7f17030d4be4c7a0614c7a858d02802
aklosovich <aklosovich@neoline.biz>
2013-10-27 16:33:51 +0200

    вывод страниц. фикстуры. категории контента

---
+++ src/Application/Project/MainBundle/Entity/ContentCategory.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Application\Project\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Application\Project\MainBundle\Entity\Content;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * ContentCategory
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class ContentCategory
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name_en", type="string", length=255)
+     */
+    private $nameEn;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Content", mappedBy="category")
+     */
+    private $contents;
+
+    public function __construct() {
+        $this->contents = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return ContentCategory
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nameEn
+     *
+     * @param string $nameEn
+     * @return ContentCategory
+     */
+    public function setNameEn($nameEn)
+    {
+        $this->nameEn = $nameEn;
+    
+        return $this;
+    }
+
+    /**
+     * Get nameEn
+     *
+     * @return string 
+     */
+    public function getNameEn()
+    {
+        return $this->nameEn;
+    }
+}
