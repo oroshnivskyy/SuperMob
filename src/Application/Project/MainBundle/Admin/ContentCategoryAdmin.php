18fda9a5fe73c3b27ff6071b2dea9339004f7c8d
oroshnivskyy <oleg.roshnivsky@gmail.com>
2013-11-04 22:11:58 +0200

    Added ContentCategoryAdmin, fixed url in pageadmin

---
+++ src/Application/Project/MainBundle/Admin/ContentCategoryAdmin.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Application\Project\MainBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class ContentCategoryAdmin extends Admin{
+    /**
+     * Конфигурация формы редактирования записи
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', null, array('label' => 'Название'))
+            ->add('nameEn', null, array('label' => 'Название на английском'));
+    }
+
+    /**
+     * Конфигурация списка записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+            ->addIdentifier('name', null, array('label' => 'Название'))
+            ->addIdentifier('nameEn', null, array('label' => 'Название на английском'));
+
+    }
+
+    /**
+     * Поля, по которым производится поиск в списке записей
+     *
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name', null, array('label' => 'Название'));
+    }
+}
+ 
