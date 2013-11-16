2084e96d08514a23399219a47e7e3faa97e9d55a
oroshnivskyy <oleg.roshnivsky@gmail.com>
2013-11-16 12:21:04 +0200

    bugfix

--- src/Application/Project/MainBundle/Controller/PageController.php
+++ src/Application/Project/MainBundle/Controller/PageController.php
@@ -54,13 +54,11 @@ class PageController extends Controller
         $webPath = $this->container->getParameter('slider_upload_dir');
         $webPathContent = $this->container->getParameter('content_upload_dir');
 
-        $payment = $this->get('payment.mainpay');
         return array(
             'entity'      => $entity,
             'contents'    => $contents,
             'webPathContent' => $webPathContent,
             'webPathSlider' => $webPath,
-            'paymentService' => $payment,
             'user' => $this->getUser()
         );
     }
