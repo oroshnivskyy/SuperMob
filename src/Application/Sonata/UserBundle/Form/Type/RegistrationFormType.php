6f52d35fa0610c4eca1c78cd0d8c92fa2771f4db
oroshnivskyy <oleg.roshnivsky@gmail.com>
2013-12-08 20:28:36 +0200

    F

--- src/Application/Sonata/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Application/Sonata/UserBundle/Form/Type/RegistrationFormType.php
@@ -3,6 +3,7 @@ namespace Application\Sonata\UserBundle\Form\Type;
 
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+use Doctrine\ORM\EntityRepository;
 
 class RegistrationFormType extends BaseType
 {
@@ -10,25 +11,22 @@ class RegistrationFormType extends BaseType
     {
         parent::buildForm($builder, $options);
         $builder
-//            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
-//            ->add('email', 'email', array('label' => '123', 'translation_domain' => 'FOSUserBundle'))
             ->add('country', 'choice',
                   array('label' => 'Страна',
                         'expanded' => 'radio',
-//                        'requer' => true,
+                        'preferred_choices' => [2],
                         'choices' =>
                         array(
                             '2' => 'Украина',
                             '1' => 'Россия'
-                        )))
-            ->add('operator', 'hidden',array())
-//            ->add('plainPassword', 'repeated', array(
-//                'type' => 'password',
-//                'options' => array('translation_domain' => 'FOSUserBundle'),
-//                'first_options' => array('label' => 'form.password'),
-//                'second_options' => array('label' => 'form.password_confirmation'),
-//                'invalid_message' => 'fos_user.password.mismatch',
-//            ))
+                        )));
+        $country = $builder->getAttribute('country',2);
+        $builder->add('operator', 'entity',
+                array(
+                        'class'=> 'MainBundle:Operator',
+                        'property' => 'name',
+                        'label' => 'Оператор',
+                   ))
         ;
     }
 
